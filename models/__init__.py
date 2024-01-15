362712d7ce5663bebd4b76238e4ad26d61e1aef5
doha-rkb <rakibdouha2002@gmail.com>
2024-01-15 13:47:38 +0100

    my commit

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import Books_data
+from . import publisher
+from . import Author
+from . import Borrows
+from . import Book_copies
+from . import Book_Category
+
+
